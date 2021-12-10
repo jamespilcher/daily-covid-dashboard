f79b7454f0f2ee1893ae1fd8a82bcfb9c9232cc2
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2021-12-10 00:59:44 +0000

    Update test_interface.py

--- Covid Dashboard/test_interface.py
+++ Covid Dashboard/test_interface.py
@@ -1,12 +1,12 @@
 """Tests the functions within the module: interface"""
-#from time import gmtime
 
 from interface import random_words
-#from interface import time_until_activate
+from interface import time_until_activate
 
 def test_random_words():
     """Tests random_words"""
     random_words()
+
 def test_time_until_activate():
     """Tests time_until_activate"""
     time_until_activate("00:00")
