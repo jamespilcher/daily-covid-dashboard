0defdafd35e3966e81cbb1db38979302f65f6931
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2021-12-10 00:59:07 +0000

    Update test_interface.py

--- Covid Dashboard/test_interface.py
+++ Covid Dashboard/test_interface.py
@@ -7,3 +7,6 @@ from interface import random_words
 def test_random_words():
     """Tests random_words"""
     random_words()
+def test_time_until_activate():
+    """Tests time_until_activate"""
+    time_until_activate("00:00")
